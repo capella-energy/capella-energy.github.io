3aac6bcb2ccb5e96ae5379039b8b32d457e0ddf3
capella-energy <73075284+capella-energy@users.noreply.github.com>
2020-10-20 18:38:29 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -14,31 +14,4 @@ if($_POST["submit"]) {
     $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
 
-?><!DOCTYPE html>
-
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Contact form to email</title>
-</head>
-
-<body>
-
-    <?=$thankYou ?>
-
-    <form method="post" action="contact.php">
-        <label>Name:</label>
-        <input name="sender">
-
-        <label>Email address:</label>
-        <input name="senderEmail">
-
-        <label>Message:</label>
-        <textarea rows="5" cols="20" name="message"></textarea>
-
-        <input type="submit" name="submit">
-    </form>
-
-</body>
-
-</html>
+?>
